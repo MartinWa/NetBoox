4f7ceb2b8ebf08a51a5d0db5170f0591e020e5f0
Martin Wåger <martinw@gmail.com>
2013-12-02 11:54:53 +0100

    Added resource files and l18n of BookViewModel

--- NetBoox/ViewModels/BookViewModel.cs
+++ NetBoox/ViewModels/BookViewModel.cs
@@ -1,25 +1,28 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
+using Resources;
 
 namespace NetBoox.ViewModels
 {
     public class BookViewModel
     {
         public int BookId { get; set; }
-        [Required]
-        [StringLength(160)]
+        [Display(Name = "Title", ResourceType = typeof(Language))]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(Language))]
+        [StringLength(160, ErrorMessageResourceName = "FieldMaxLength160", ErrorMessageResourceType = typeof(Language))]
         public string Title { get; set; }
-        [Required]
-        [StringLength(160)]
+        [Display(Name = "Author", ResourceType = typeof(Language))]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(Language))]
+        [StringLength(160, ErrorMessageResourceName = "FieldMaxLength160", ErrorMessageResourceType = typeof(Language))]
         public string Author { get; set; }
-        [Required]
-        [Range(1, 5)]
+        [Display(Name = "Rating", ResourceType = typeof(Language))]
+        [Required(ErrorMessageResourceName = "FieldRequired", ErrorMessageResourceType = typeof(Language))]
+        [Range(1, 5, ErrorMessageResourceName = "Range1to5", ErrorMessageResourceType = typeof(Language))]
         public int Rating { get; set; }
         [Required]
         public int GenreId { get; set; }
-        [DisplayName("Genre")]
+        [Display(Name = "Genre", ResourceType = typeof(Language))]
         public string GenreName { get; set; }
         public IEnumerable<SelectListItem> GenreList { get; set; }
     }
